2cc73ea41172107c4f46a110b1048a712332b259
Rudia Kim <rudiakim@usc.edu>
2018-01-20 19:14:44 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,10 +1,13 @@
 def position_taken?(board, index)
   if index == " "
     false
+
+  elsif index != " "
+    true
+
   elsif index == "X" || index == "O"
     true
-  else index != " "
-    false
+
 
 end
 end
